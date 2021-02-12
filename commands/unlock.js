64b27a3a74342f7a1db081b177b54dfac56176b1
bottutorial <78550309+bottutorial@users.noreply.github.com>
2021-02-12 20:55:28 -0300

    unlock.js

---
+++ commands/unlock.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js")
+const db = require("quick.db")
+module.exports.run = async(client,message,args)=> {
+
+    if(!message.member.hasPermission("MANAGE_CHANNELS")) return;
+    
+    
+
+    if(!db.fetch(`lock.${message.channel.id}`)) return message.reply("canal ja desbloqueado")
+
+    let msg = await message.channel.send("Loading...")
+
+    try {
+        db.delete(`lock.${message.channel.id}`)
+        message.channel.updateOverwrite(message.guild.roles.cache.find(e => e.name.toLowerCase().trim() == "@everyone"),{
+            SEND_MESSAGES:true,
+            ADD_REACTIONS:true
+        })
+        msg.edit(`${message.author}🔓| canal aberto`)
+
+    }catch(e){
+        message.channel.send(e)
+    }
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    aliase:[]
+}
+
+exports.help = {
+    name:"unlock",
+    description:"Empty",
+    usage:"embed",
+    category:"moderation"
+}
